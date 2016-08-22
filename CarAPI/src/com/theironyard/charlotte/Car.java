10c561c2de8e1a6b836f2baab068a91c2cce0bde
wrodgers95 <will.rodgers1995@gmail.com>
2016-08-22 16:36:21 -0400

    initial commmit

---
+++ CarAPI/src/com/theironyard/charlotte/Car.java
@@ -0,0 +1,39 @@
+package com.theironyard.charlotte;
+
+public class Car {
+
+    String color;
+    String model;
+    int year;
+
+    public Car (String color, String model, int year){
+
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ CarAPI/src/com/theironyard/charlotte/Main.java
@@ -0,0 +1,13 @@
+package com.theironyard.charlotte;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+        private static ArrayList<Car> cars = new ArrayList<>();
+        private static Gson gson = new Gson();
+
+        public static void main(String[] args
+}
